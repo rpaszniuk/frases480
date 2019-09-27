d7ff403b1e8af4852b79b4e52a8341eaac4162ef
Rodrigo Paszniuk <rodrigpaszniuk@gmail.com>
2019-09-26 22:08:09 -0400

    Change engine

--- db/migrate/20190823225344_create_active_storage_tables.active_storage.rb
+++ db/migrate/20190823225344_create_active_storage_tables.active_storage.rb
@@ -1,7 +1,7 @@
 # This migration comes from active_storage (originally 20170806125915)
-class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
+class CreateActiveStorageTables < ActiveRecord::Migration[6.0]
   def change
-    create_table :active_storage_blobs do |t|
+    create_table :active_storage_blobs, options: 'ENGINE=MyISAM DEFAULT CHARSET=utf8' do |t|
       t.string   :key,        null: false
       t.string   :filename,   null: false
       t.string   :content_type
@@ -13,14 +13,14 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
       t.index [ :key ], unique: true
     end
 
-    create_table :active_storage_attachments do |t|
+    create_table :active_storage_attachments, options: 'ENGINE=MyISAM DEFAULT CHARSET=utf8' do |t|
       t.string     :name,     null: false
       t.references :record,   null: false, polymorphic: true, index: false
       t.references :blob,     null: false
 
       t.datetime :created_at, null: false
 
-      t.index [ :record_type, :record_id, :name, :blob_id ], name: "index_active_storage_attachments_uniqueness", unique: true
+      t.index [ :record_type, :record_id, :name, :blob_id ], name: 'index_active_storage_attachments_uniqueness', unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
     end
   end
