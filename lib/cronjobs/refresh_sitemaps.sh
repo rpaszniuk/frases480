0bdd1142cd83a5dd970a7df9bcea5d09f3d82d55
Rodrigo Paszniuk <rodrigpaszniuk@gmail.com>
2019-09-27 08:57:43 -0400

    Update cron

--- lib/cronjobs/refresh_sitemaps.sh
+++ lib/cronjobs/refresh_sitemaps.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# Crontab: 0 3 * * * /path/to/app/lib/cronjobs/refresh_sitemaps.sh
+# Crontab: 0 */12 * * * /path/to/app/lib/cronjobs/refresh_sitemaps.sh
 
 # load rvm ruby
-source /home/ubuntu/.rvm/environments/default
+source /home/rodrigo/.rvm/environments/default
 
-cd /var/www/html/production_frases480/
+cd /var/www/html/newfrases480/
 rake -s sitemap:refresh RAILS_ENV=production
