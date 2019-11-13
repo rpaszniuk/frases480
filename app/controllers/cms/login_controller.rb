9ec77c89d7143273609b9f198395c1ee57e770db
Rodrigo Paszniuk <rodrigopaszniuk@gmail.com>
2019-11-13 17:33:01 -0300

    Fix

--- app/controllers/cms/login_controller.rb
+++ app/controllers/cms/login_controller.rb
@@ -1,19 +1,11 @@
 class CMS::LoginController < ApplicationController
   def index
     unless session[:sess_user].nil?
-      user = User.includes(:access_profile).find_by(id: session[:sess_user], status: User.statuses[:active])
-      if !user.nil?
-        reset_session
-        session[:sess_user] = nil
-        redirect_to :cms_root
-      else
-        redirect_to :cms_dashboard
-      end
+      redirect_to :cms_dashboard
       return
     end
 
     @user_auth = UserAuth.new
-
     render layout: 'cms_empty'
   end
 end
